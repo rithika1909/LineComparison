496c607b7892b50d00abd18a98a662c4b3208715
Rithika L <rithika.l@tvsnext.com>
2023-07-12 15:02:34 +0530

    [Rithika] Add : Implemented

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -6,6 +6,14 @@ namespace LineComparison
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Problem");
+            Operation operation1 = new Operation(2, 1, 4, 3);
+            Operation operation2 = new Operation(6, 4, 2, 3);
+            double line1= operation1.CalculateLength();
+            double line2 = operation2 .CalculateLength(); 
+            if (line1.Equals(line2))
+                Console.WriteLine("Line1 and Line2 are equal");
+            else
+                Console.WriteLine("Line1 and Line2 are not equal");
         }
     }
 
