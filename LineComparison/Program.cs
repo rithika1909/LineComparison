2260cf1b9653955910ace9c6c094d3cb7b7280d4
rithika1909 <113461039+rithika1909@users.noreply.github.com>
2023-07-17 16:42:37 +0530

    Update Program.cs

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -10,12 +10,12 @@ namespace LineComparison
             Operation operation2 = new Operation(6, 4, 2, 3);
             double line1= operation1.CalculateLength();
             double line2 = operation2 .CalculateLength(); 
-            //Equals -UC2
+            
             if (line1.Equals(line2))
                 Console.WriteLine("Line1 and Line2 are equal");
             else
                 Console.WriteLine("Line1 and Line2 are not equal");
-            //CompareTo - UC3
+        
             if (line1.CompareTo(line2) == 0)
                 Console.WriteLine("Both Line Are Equal");
             if (line1.CompareTo(line2) > 0)
